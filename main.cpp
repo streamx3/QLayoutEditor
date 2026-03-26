7ac18e565d0d556e018add632a6cf78ae76ef8ca
Andrii Shelestov <streamx3@gmail.com>
2026-03-27 01:30:20 +0200

    Minor updates, release 0.1.0

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setOrganizationName("streamx3");
     a.setApplicationName("QLayoutEditor");
+    a.setApplicationVersion("0.1.0");
     a.setWindowIcon(QIcon(":/QLayoutEditor.svg"));
     MainWindow w;
     w.show();
